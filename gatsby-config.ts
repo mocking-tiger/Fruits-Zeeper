9e1d40885b5867ce62f18523cb573344e5f49c8f
Park JunYong <dyden119@gmail.com>
2025-01-24 22:14:42 +0900

    chore: useStaticQuery

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,17 @@
 import type { GatsbyConfig } from "gatsby";
+import { title } from "process";
 
 const config: GatsbyConfig = {
   siteMetadata: {
+    title: "FRUITS ZEEPER!",
     siteUrl: `https://www.yourdomain.tld`,
+    description: "딸기 다판다 포도 다판다",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [
-    {
-      resolve: `gatsby-plugin-babel`,
-      options: {
-        presets: [`@babel/preset-react`],
-      },
-    },
-  ],
+  plugins: [],
 };
 
 export default config;
--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,7 +1,23 @@
+import { graphql, useStaticQuery } from "gatsby";
+
 interface ISeoProps {
   title: string;
 }
 
 export default function Seo({ title }: ISeoProps) {
-  return <title>{title} | FRUITS ZEEPER!</title>;
+  // 스태틱 쿼리는 빌드단계에서 html을 만들때 생성됨
+  const data = useStaticQuery<Queries.SeoDataQuery>(graphql`
+    query SeoData {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  return (
+    <title>
+      {title} | {data.site?.siteMetadata?.title}
+    </title>
+  );
 }
