5c06866c06744bc9fe5173e43fe4cf9152163a30
Park JunYong <dyden119@gmail.com>
2025-01-25 14:50:37 +0900

    chore: page query 설정

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,12 +1,29 @@
+import { graphql, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTitleQuery>) {
+  console.log(data);
   return (
     <Layout title="후르츠지퍼 블로그">
       <p>후르츠지퍼 새소식</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTitle {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="BLOG" />;
