65667fb527a5b53eae2dffdfc1216873564f7671
Park JunYong <dyden119@gmail.com>
2025-01-25 16:02:08 +0900

    chore: mdx

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,25 +2,42 @@ import { graphql, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-export default function Blog({ data }: PageProps<Queries.BlogTitleQuery>) {
+export default function Blog({ data }: PageProps<Queries.BlogPostsQuery>) {
   console.log(data);
   return (
     <Layout title="후르츠지퍼 블로그">
       <p>후르츠지퍼 새소식</p>
-      <ul>
-        {data.allFile.nodes.map((file, index) => (
-          <li key={index}>{file.name}</li>
+      <section>
+        {data.allMdx.nodes.map((file, index) => (
+          <article
+            key={index}
+            style={{ border: "1px solid black", marginBottom: "10px" }}
+          >
+            <h3>{file.frontmatter?.title}</h3>
+            <h5>
+              {file.frontmatter?.author} in : {file.frontmatter?.category}
+            </h5>
+            <h6>{file.frontmatter?.date}</h6>
+            <hr />
+            <p>{file.excerpt}</p>
+          </article>
         ))}
-      </ul>
+      </section>
     </Layout>
   );
 }
 
 export const query = graphql`
-  query BlogTitle {
-    allFile {
+  query BlogPosts {
+    allMdx {
       nodes {
-        name
+        frontmatter {
+          author
+          category
+          date(formatString: "YYYY.MM.DD")
+          title
+        }
+        excerpt
       }
     }
   }
